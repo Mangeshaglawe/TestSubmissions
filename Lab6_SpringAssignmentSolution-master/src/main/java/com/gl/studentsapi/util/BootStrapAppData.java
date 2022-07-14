4345e7a401a77d3dc0953cd759a8c3184ec134aa
Mangesh <Mangeshjaglawe@gmail.com>
2022-07-14 20:21:33 +0530

    Lab6

---
+++ Lab6_SpringAssignmentSolution-master/src/main/java/com/gl/studentsapi/util/BootStrapAppData.java
@@ -0,0 +1,65 @@
+package com.gl.studentsapi.util;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.gl.studentsapi.model.Role;
+import com.gl.studentsapi.model.Student;
+import com.gl.studentsapi.model.User;
+import com.gl.studentsapi.repository.RoleRepository;
+import com.gl.studentsapi.repository.StudentRepository;
+import com.gl.studentsapi.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class BootStrapAppData implements ApplicationListener<ApplicationReadyEvent>{
+	
+	private final StudentRepository studentRepository;
+	private final RoleRepository roleRepository;
+	private final UserRepository userRepository;
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		// implement faker here
+		for(int i=0; i<10; i++) {
+			Student student = new Student();
+			student.setFirstName("Sameer" + i);
+			student.setLastName("asd" + i);
+			student.setCountry("In" + i);
+			student.setCourse("IT" + i);
+			studentRepository.save(student);
+		}
+		
+		
+		Role userRole=new Role();
+		userRole.setRoleName("ROLE_USER");
+		
+		Role adminRole=new Role();
+		adminRole.setRoleName("ROLE_ADMIN");
+		
+		User user=new User();
+		user.setUserName("ravi");
+		user.setPassword(this.passwordEncoder .encode("test123"));
+		
+		User admin=new User();
+		admin.setUserName("satya");
+		admin.setPassword(this.passwordEncoder .encode("testadmin"));
+				
+		admin.addRole(adminRole);
+		admin.addRole(userRole);
+		
+		user.addRole(userRole);
+		
+		userRepository.save(user);
+		userRepository.save(admin);
+		roleRepository.save(userRole);
+		roleRepository.save(adminRole);
+	}
+
+}
